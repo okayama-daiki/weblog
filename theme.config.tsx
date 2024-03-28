5e14adea190ff397b5d858a6bfe1a8e7e2538393
Daiki Okayama <lawliet.d.k.m@gmail.com>
2024-03-29 03:45:37 +0900

    Configure site appearance and rendering settings

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,33 @@
-const config = {
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
   logo: <span>Weblog</span>,
   project: {
     link: "https://github.com/okayama-daiki/weblog",
+  },
+  docsRepositoryBase: "https://github.com/okayama-daiki/weblog/tree/main",
+  banner: {
+    key: "banner",
+    text: "🚧 This website is under construction. 🚧",
+  },
+  toc: {
+    backToTop: true,
+  },
+  feedback: {
+    content: null,
+  },
+  navigation: false,
+  gitTimestamp: null,
+  footer: {
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} ©{" "}
+        <a href="https://daiki.dev" target="_blank">
+          Daiki Okayama
+        </a>
+        .
+      </span>
+    ),
   },
 };
 
